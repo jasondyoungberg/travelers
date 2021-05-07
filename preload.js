71cd7c4ec34576582a652cfbb39d02477307378c
Jason Youngberg <jasondyoungberg@gmail.com>
2021-05-06 19:49:55 -0600

    Add wav back as fallback

--- preload.js
+++ preload.js
@@ -1,31 +1,31 @@
 var songs = [
 	new Howl({
-		src:['resources/chert.ogg'],
+		src:['resources/chert.ogg','resources/chert.wav'],
 		loop: true,
 		volume: 0
 	}),
 	new Howl({
-		src:['resources/esker.ogg'],
+		src:['resources/esker.ogg','resources/esker.wav'],
 		loop: true,
 		volume: 0
 	}),
 	new Howl({
-		src:['resources/riebeck.ogg'],
+		src:['resources/riebeck.ogg','resources/riebeck.wav'],
 		loop: true,
 		volume: 0
 	}),
 	new Howl({
-		src:['resources/gabbro.ogg'],
+		src:['resources/gabbro.ogg','resources/gabbro.wav'],
 		loop: true,
 		volume: 0
 	}),
 	new Howl({
-		src:['resources/feldspar.ogg'],
+		src:['resources/feldspar.ogg','resources/feldspar.wav'],
 		loop: true,
 		volume: 0
 	}),
 	new Howl({
-		src:['resources/solanum.ogg'],
+		src:['resources/solanum.ogg','resources/solanum.wav'],
 		loop: true,
 		volume: 0
 	})
